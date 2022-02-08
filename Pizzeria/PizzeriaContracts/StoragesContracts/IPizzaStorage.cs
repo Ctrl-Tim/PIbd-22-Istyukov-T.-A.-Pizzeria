36f8ab119b90617a01686c577df6a7b002f3d02a
Ctrl-Tim <timon.istiukov@mail.ru>
2022-02-08 17:42:38 +0400

    commit 6

---
+++ Pizzeria/PizzeriaContracts/StoragesContracts/IPizzaStorage.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using PizzeriaContracts.BindingModels;
+using PizzeriaContracts.ViewModels;
+
+namespace PizzeriaContracts.StoragesContracts
+{
+    public interface IPizzaStorage
+    {
+        List<PizzaViewModel> GetFullList();
+
+        List<PizzaViewModel> GetFilteredList(PizzaBindingModel model);
+
+        PizzaViewModel GetElement(PizzaBindingModel model);
+
+        void Insert(PizzaBindingModel model);
+
+        void Update(PizzaBindingModel model);
+
+        void Delete(PizzaBindingModel model);
+    }
+}
