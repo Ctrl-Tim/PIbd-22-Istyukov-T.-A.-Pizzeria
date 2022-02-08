7c18d19aac9a465e93070ac19ba6ff0ec3457d26
Ctrl-Tim <timon.istiukov@mail.ru>
2022-02-08 17:42:31 +0400

    commit 5

---
+++ Pizzeria/PizzeriaContracts/BusinessLogicsContracts/IOrderLogic.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using PizzeriaContracts.BindingModels;
+using PizzeriaContracts.ViewModels;
+
+namespace PizzeriaContracts.BusinessLogicsContracts
+{
+    public interface IOrderLogic
+    {
+        List<OrderViewModel> Read(OrderBindingModel model);
+
+        void CreateOrder(CreateOrderBindingModel model);
+
+        void TakeOrderInWork(ChangeStatusBindingModel model);
+
+        void FinishOrder(ChangeStatusBindingModel model);
+
+        void DeliveryOrder(ChangeStatusBindingModel model);
+    }
+}
---
+++ Pizzeria/PizzeriaContracts/BusinessLogicsContracts/IPizzaLogic.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using PizzeriaContracts.BindingModels;
+using PizzeriaContracts.ViewModels;
+
+namespace PizzeriaContracts.BusinessLogicsContracts
+{
+    public interface IPizzaLogic
+    {
+        List<PizzaViewModel> Read(PizzaBindingModel model);
+
+        void CreateOrUpdate(PizzaBindingModel model);
+
+        void Delete(PizzaBindingModel model);
+    }
+}
