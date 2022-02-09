f06545951a9f6b1b5e01f97adbbb9f37ffa6fdb1
Ctrl-Tim <timon.istiukov@mail.ru>
2022-02-10 02:48:14 +0400

    commit 14

---
+++ Pizzeria/PizzeriaView/FormCreateOrder.Designer.cs
@@ -0,0 +1,150 @@
+﻿
+namespace PizzeriaView
+{
+    partial class FormCreateOrder
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBoxPizza = new System.Windows.Forms.ComboBox();
+            this.textBoxCount = new System.Windows.Forms.TextBox();
+            this.textBoxSum = new System.Windows.Forms.TextBox();
+            this.ButtonCancel = new System.Windows.Forms.Button();
+            this.ButtonSave = new System.Windows.Forms.Button();
+            this.labelPizza = new System.Windows.Forms.Label();
+            this.labelCount = new System.Windows.Forms.Label();
+            this.labelSum = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboBoxPizza
+            // 
+            this.comboBoxPizza.FormattingEnabled = true;
+            this.comboBoxPizza.Location = new System.Drawing.Point(92, 12);
+            this.comboBoxPizza.Name = "comboBoxPizza";
+            this.comboBoxPizza.Size = new System.Drawing.Size(286, 23);
+            this.comboBoxPizza.TabIndex = 0;
+            this.comboBoxPizza.SelectedIndexChanged += new System.EventHandler(this.ComboBoxPizza_SelectedIndexChanged);
+            // 
+            // textBoxCount
+            // 
+            this.textBoxCount.AcceptsTab = true;
+            this.textBoxCount.Location = new System.Drawing.Point(92, 41);
+            this.textBoxCount.Name = "textBoxCount";
+            this.textBoxCount.Size = new System.Drawing.Size(286, 23);
+            this.textBoxCount.TabIndex = 1;
+            this.textBoxCount.TextChanged += new System.EventHandler(this.TextBoxCount_TextChanged);
+            // 
+            // textBoxSum
+            // 
+            this.textBoxSum.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.textBoxSum.Location = new System.Drawing.Point(92, 71);
+            this.textBoxSum.Name = "textBoxSum";
+            this.textBoxSum.Size = new System.Drawing.Size(286, 23);
+            this.textBoxSum.TabIndex = 2;
+            // 
+            // ButtonCancel
+            // 
+            this.ButtonCancel.Location = new System.Drawing.Point(287, 100);
+            this.ButtonCancel.Name = "ButtonCancel";
+            this.ButtonCancel.Size = new System.Drawing.Size(91, 33);
+            this.ButtonCancel.TabIndex = 3;
+            this.ButtonCancel.Text = "Отмена";
+            this.ButtonCancel.UseVisualStyleBackColor = true;
+            this.ButtonCancel.Click += new System.EventHandler(this.ButtonCancel_Click);
+            // 
+            // ButtonSave
+            // 
+            this.ButtonSave.Location = new System.Drawing.Point(190, 100);
+            this.ButtonSave.Name = "ButtonSave";
+            this.ButtonSave.Size = new System.Drawing.Size(91, 33);
+            this.ButtonSave.TabIndex = 4;
+            this.ButtonSave.Text = "Сохранить";
+            this.ButtonSave.UseVisualStyleBackColor = true;
+            this.ButtonSave.Click += new System.EventHandler(this.ButtonSave_Click);
+            // 
+            // labelPizza
+            // 
+            this.labelPizza.AutoSize = true;
+            this.labelPizza.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.labelPizza.Location = new System.Drawing.Point(8, 13);
+            this.labelPizza.Name = "labelPizza";
+            this.labelPizza.Size = new System.Drawing.Size(48, 17);
+            this.labelPizza.TabIndex = 5;
+            this.labelPizza.Text = "Пицца:";
+            // 
+            // labelCount
+            // 
+            this.labelCount.AutoSize = true;
+            this.labelCount.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.labelCount.Location = new System.Drawing.Point(8, 44);
+            this.labelCount.Name = "labelCount";
+            this.labelCount.Size = new System.Drawing.Size(81, 17);
+            this.labelCount.TabIndex = 6;
+            this.labelCount.Text = "Количество:";
+            // 
+            // labelSum
+            // 
+            this.labelSum.AutoSize = true;
+            this.labelSum.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.labelSum.Location = new System.Drawing.Point(8, 73);
+            this.labelSum.Name = "labelSum";
+            this.labelSum.Size = new System.Drawing.Size(50, 17);
+            this.labelSum.TabIndex = 7;
+            this.labelSum.Text = "Сумма:";
+            // 
+            // FormCreateOrder
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(390, 145);
+            this.Controls.Add(this.labelSum);
+            this.Controls.Add(this.labelCount);
+            this.Controls.Add(this.labelPizza);
+            this.Controls.Add(this.ButtonSave);
+            this.Controls.Add(this.ButtonCancel);
+            this.Controls.Add(this.textBoxSum);
+            this.Controls.Add(this.textBoxCount);
+            this.Controls.Add(this.comboBoxPizza);
+            this.Name = "FormCreateOrder";
+            this.Text = "Заказ";
+            this.Load += new System.EventHandler(this.FormCreateOrder_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBoxPizza;
+        private System.Windows.Forms.TextBox textBoxCount;
+        private System.Windows.Forms.TextBox textBoxSum;
+        private System.Windows.Forms.Button ButtonCancel;
+        private System.Windows.Forms.Button ButtonSave;
+        private System.Windows.Forms.Label labelPizza;
+        private System.Windows.Forms.Label labelCount;
+        private System.Windows.Forms.Label labelSum;
+    }
+}
