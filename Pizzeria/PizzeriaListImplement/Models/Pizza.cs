f140af84cf3fd8b62ba3907b4f298b255a2d444a
Ctrl-Tim <timon.istiukov@mail.ru>
2022-02-10 02:12:08 +0400

    commit 11

---
+++ Pizzeria/PizzeriaListImplement/Models/Pizza.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace PizzeriaListImplement.Models
+{
+    /// <summary>
+    /// Пицца, изготавливаемая в пиццерии
+    /// </summary>   
+    public class Pizza
+    {
+        public int Id { get; set; }
+
+        public string PizzaName { get; set; }
+
+        public decimal Price { get; set; }
+
+        public Dictionary<int, int> PizzaIngredients { get; set; }
+    }
+}
