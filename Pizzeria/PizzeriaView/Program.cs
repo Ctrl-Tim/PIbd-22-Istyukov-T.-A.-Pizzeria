4b95d82050e5dce707d64c9004954be6abadd534
Ctrl-Tim <timon.istiukov@mail.ru>
2022-02-10 02:48:24 +0400

    commit 15

--- Pizzeria/PizzeriaView/Program.cs
+++ Pizzeria/PizzeriaView/Program.cs
@@ -1,13 +1,19 @@
+using PizzeriaBusinessLogic.BusinessLogics;
+using PizzeriaContracts.BusinessLogicsContracts;
+using PizzeriaContracts.StoragesContracts;
+using PizzeriaListImplement.Implements;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using Unity;
+using Unity.Lifetime;
 
 namespace PizzeriaView
 {
     static class Program
     {
+        private static IUnityContainer container = null;
+
+        public static IUnityContainer Container { get { if (container == null) { container = BuildUnityContainer(); } return container; } }
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
@@ -17,7 +23,22 @@ namespace PizzeriaView
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(Container.Resolve<FormMain>());
+        }
+
+        private static IUnityContainer BuildUnityContainer()
+        {
+            var currentContainer = new UnityContainer();
+
+            currentContainer.RegisterType<IIngredientStorage, IngredientStorage>(new HierarchicalLifetimeManager());
+            currentContainer.RegisterType<IOrderStorage, OrderStorage>(new HierarchicalLifetimeManager());
+            currentContainer.RegisterType<IPizzaStorage, PizzaStorage>(new HierarchicalLifetimeManager());
+
+            currentContainer.RegisterType<IIngredientLogic, IngredientLogic>(new HierarchicalLifetimeManager());
+            currentContainer.RegisterType<IOrderLogic, OrderLogic>(new HierarchicalLifetimeManager());
+            currentContainer.RegisterType<IPizzaLogic, PizzaLogic>(new HierarchicalLifetimeManager());
+
+            return currentContainer;
         }
     }
 }
